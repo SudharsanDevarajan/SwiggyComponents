4b09466d89c818a394e7649acdd324298c31b412
sudharsan <sudharsan.devarajan@htcinc.com>
2022-11-23 16:00:34 +0530

    Initial Commit

---
+++ SwiggyComponents/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiggyComponents
+//
+//  Created by htcuser on 23/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiggyComponents/SwiggyComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiggyComponentsApp.swift
+//  SwiggyComponents
+//
+//  Created by htcuser on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiggyComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
